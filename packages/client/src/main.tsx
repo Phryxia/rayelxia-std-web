7eb602b66d6915c10b8ebed545f41975ccf100e4
Phryxia <xahhaepica@gmail.com>
2026-07-23 03:12:51 +0900

    fix: remove hash routing
    this causes several problem especially incomming with url

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createHashHistory, createRouter, RouterProvider } from '@tanstack/react-router'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '@picocss/pico/css/pico.conditional.min.css'
 import './global.css'
@@ -13,7 +13,7 @@ declare module '@tanstack/react-router' {
 
 import { routeTree } from './routeTree.gen'
 
-const router = createRouter({ routeTree, history: createHashHistory() })
+const router = createRouter({ routeTree })
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
